1eaa4eebc977697e0ec79af582a96a177f3796f9
Brad <Brad@dyn101072.cc.lehigh.edu>
2018-04-25 11:23:16 -0400

    testing

--- src/Node.java
+++ src/Node.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 /**
  * 
+<<<<<<< HEAD
+ * @author Brad Kelly 
+ * @version 2018.04.24
+=======
  * @author Matt Charno
  * @author Cyrus Johannes
  * @author Brad Kelly
  * @version 2018.04.24
  *
+>>>>>>> branch 'master' of https://github.com/bkelly0087/Final-Project.git
  * @param <T>
  */
 public class Node<T> {
 
     private T element;
     private ArrayList<String> list;
+    
     /**
      * constructor for the node
      * @param l element for node
@@ -21,6 +27,7 @@ public class Node<T> {
         list = new ArrayList<String>();
         element = l;
     }
+    
     /**
      * getter for the element
      * @return element
@@ -28,6 +35,7 @@ public class Node<T> {
     public T getElement() {
         return element;
     }
+    
     /**
      * setter for the element
      * @param value value to set
@@ -35,6 +43,7 @@ public class Node<T> {
     public void setElement(T value) {
         element = value;
     }
+    
     /**
      * Getter of the array list
      * @return ArrayList 
@@ -42,6 +51,7 @@ public class Node<T> {
     public ArrayList<String> getList(){
         return list;
     }
+    
     /**
      * setter for the array list
      * @param l sets equal to list
@@ -52,6 +62,17 @@ public class Node<T> {
     /**
      * to string method for the array list
      */
+    /**
+     * Returns the node as a string
+     * @return the string
+     */
+    public String toStringCy() {
+        if (this.list == null) { // null
+            return "null";
+        }
+        return this.list.toString();
+    }
+
     public String toString() {
         String result = " ";
         for (int i = 0; i< this.list.size(); i++) {
